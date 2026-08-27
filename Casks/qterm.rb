9c39b70af99ba22506da3cc7e0bfff41f8dcfe73
qiubai-lab <qiubaistudio@gmail.com>
2026-08-27 09:12:01 +0800

    Update Qterm to version 0.2.11

--- Casks/qterm.rb
+++ Casks/qterm.rb
@@ -1,13 +1,13 @@
 cask "qterm" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2.10"
+  version "0.2.11"
 
   on_arm do
-    sha256 "806146835564aaedbf7f724bd16a9e27e226ca1b14a614ca8f69e50f3697e471"
+    sha256 "3298ee28da7056e9fe3e6a68a94d884af5f97124be71d7b5b130483b05961047"
   end
   on_intel do
-    sha256 "b7087538f12e9f4ff63d56b0c288ed3340b73b48c988748cf8c7a7519969ba23"
+    sha256 "d8e1440639da9bd82b31f98c5b0d842dac57c7d0f747e471ce54b808c7b852da"
   end
 
   url "https://github.com/qiubai-lab/Qterm/releases/download/v#{version}/Qterm_#{version}_#{arch}.dmg"
